0ee769a2034a36670466a7d0f3a4f571e87b78fe
Filip Wasilewski <en@ig.ma>
2013-02-15 23:04:41 +0100

    Add startup script and automatic loading of receiver submodules (via @pinax)

---
+++ project_name/startup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+from django.conf import settings
+from django.utils.importlib import import_module
+from django.utils.module_loading import module_has_submodule
+
+
+def autoload(submodules):
+    for app in settings.INSTALLED_APPS:
+        mod = import_module(app)
+        for submodule in submodules:
+            try:
+                import_module("{}.{}".format(app, submodule))
+            except:
+                if module_has_submodule(mod, submodule):
+                    raise
+
+
+def run():
+    autoload(["receivers"])
--- wsgi.py
+++ wsgi.py
@@ -22,6 +22,9 @@ import os
 # os.environ["DJANGO_SETTINGS_MODULE"] = "{{ project_name }}.settings"
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{ project_name }}.settings")
 
+import {{ project_name }}.startup as startup
+startup.run()
+
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
